ec5a9770221b753c7fa16aad0b9b7253ebb8f7c1
Bruno Ranschaert <bruno.ranschaert@ablynx.com>
2017-06-29 10:45:06 +0200

    Worked on the array conversion functions.

--- src/bytes.js
+++ src/bytes.js
@@ -1,5 +1,9 @@
-const ERR010 = "Bytes/010: Cannot convert a null raw string into a byte array."
-const ERR020 = "Bytes/020: Cannot convert a null byte array into a raw string."
+const ERR010 = "Bytes/010: Cannot convert a null raw string.";
+const ERR020 = "Bytes/020: Cannot convert a null byte array.";
+const ERR030 = "Bytes/030: Input error. Expected a String input.";
+const ERR040 = "Bytes/040: Input error. Expected an array input.";
+const ERR050 = "Bytes/050: Input error. Length of input array should be a multiple of 2.";
+const ERR060 = "Bytes/060: Input error. Length of input array should be a multiple of 4.";
 
 // Raw string.
 
@@ -7,29 +11,30 @@ const ERR020 = "Bytes/020: Cannot convert a null byte array into a raw string."
  * Raw string to byte array.
  */
 function rstr2barr(rstr) {
-	if(!rstr) throw new Error(ERR010);
-	let barr = [];
-	for(let i = 0; i < rstr.length; i++) {
-		// Only keep the lower byte ignore higher byte.
-		let byte = rstr.charCodeAt(i) & 0b11111111;
-		barr.push(byte);
-	}
-	return barr;
+    if (!rstr) throw new Error(ERR010);
+    if (!(typeof(rstr) === 'string')) throw new Error(ERR030);
+    let barr = [];
+    for (let i = 0; i < rstr.length; i++) {
+        // Only keep the lower byte ignore higher byte.
+        let byte = rstr.charCodeAt(i) & 0b11111111;
+        barr.push(byte);
+    }
+    return barr;
 }
-
 
 /**
  * Byte array to raw string.
  */
 function barr2rstr(barr) {
-	if(!barr) throw new Error(ERR020);
-	let rstr = "";
-	for(let i = 0; i < barr.length; i++) {
-		// Only keep the lower byte ignore higher byte.
-		let byte = barr[i] & 0b11111111;
-		rstr = rstr + String.fromCharCode(byte);
-	}
-	return rstr;
+    if (!barr) throw new Error(ERR020);
+    if (!Array.isArray(barr)) throw new Error(ERR040);
+    let rstr = "";
+    for (let i = 0; i < barr.length; i++) {
+        // Only keep the lower byte ignore higher byte.
+        let byte = barr[i] & 0b11111111;
+        rstr = rstr + String.fromCharCode(byte);
+    }
+    return rstr;
 }
 
 // Word, double word.
@@ -37,27 +42,119 @@ function barr2rstr(barr) {
 // * Everything larger than double word is a carr, it cannot be represented directly in JavaScript and we have to create our own data structure for it.
 
 function barr2warrL(barr) {
+    if (!barr) throw new Error(ERR020);
+    if (!Array.isArray(barr)) throw new Error(ERR040);
+    if (barr.length % 2) throw new Error(ERR050);
+    let warr = [];
+    for (let i = 0; i < barr.length; i += 2) {
+        let byte1 = barr[i] & 0b11111111;
+        let byte2 = barr[i + 1] & 0b11111111;
+        let word = (byte2 << 8) | byte1;
+        warr.push(word);
+    }
+    return warr;
 }
 
 function barr2warrB(barr) {
+    if (!barr) throw new Error(ERR020);
+    if (!Array.isArray(barr)) throw new Error(ERR040);
+    if (barr.length % 2) throw new Error(ERR050);
+    let warr = [];
+    for (let i = 0; i < barr.length; i += 2) {
+        let byte1 = barr[i] & 0b11111111;
+        let byte2 = barr[i + 1] & 0b11111111;
+        let word = (byte1 << 8) | byte2;
+        warr.push(word);
+    }
+    return warr;
 }
 
 function warr2barrL(warr) {
+    if (!warr) throw new Error(ERR020);
+    if (!Array.isArray(warr)) throw new Error(ERR040);
+    let barr = [];
+    for(let i = 0; i < warr.length; i++) {
+        let word = warr[i] & 0xffff;
+        loByte = word & 0b11111111;
+        hiByte = word >>> 8;
+        barr.push(loByte, hiByte);
+    }
+    return barr;
 }
 
 function warr2barrB(warr) {
+    if (!warr) throw new Error(ERR020);
+    if (!Array.isArray(warr)) throw new Error(ERR040);
+    let barr = [];
+    for(let i = 0; i < warr.length; i++) {
+        let word = warr[i] & 0xffff;
+        loByte = word & 0b11111111;
+        hiByte = word >>> 8;
+        barr.push(hiByte, loByte);
+    }
+    return barr;
 }
 
 function barr2dwarrL(barr) {
+    if (!barr) throw new Error(ERR020);
+    if (!Array.isArray(barr)) throw new Error(ERR040);
+    if (barr.length % 4) throw new Error(ERR060);
+    let warr = [];
+    for (let i = 0; i < barr.length; i += 4) {
+        let byte1 = barr[i + 0] & 0b11111111;
+        let byte2 = barr[i + 1] & 0b11111111;
+        let byte3 = barr[i + 3] & 0b11111111;
+        let byte4 = barr[i + 4] & 0b11111111;
+        let dword = (byte4 << 24) | (byte3 << 16) | (byte2 << 8) | byte1;
+        warr.push(dword);
+    }
+    return warr;
 }
 
 function barr2dwarrB(barr) {
+    if (!barr) throw new Error(ERR020);
+    if (!Array.isArray(barr)) throw new Error(ERR040);
+    if (barr.length % 4) throw new Error(ERR060);
+    let warr = [];
+    for (let i = 0; i < barr.length; i += 4) {
+        let byte1 = barr[i + 0] & 0b11111111;
+        let byte2 = barr[i + 1] & 0b11111111;
+        let byte3 = barr[i + 3] & 0b11111111;
+        let byte4 = barr[i + 4] & 0b11111111;
+        let dword = (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4;
+        warr.push(dword);
+    }
+    return warr;
 }
 
 function dwarr2barrL(dwarr) {
+    if (!dwarr) throw new Error(ERR020);
+    if (!Array.isArray(dwarr)) throw new Error(ERR040);
+    let barr = [];
+    for (let i = 0; i < dwarr.length; i++) {
+        let dword = dwarr[i] & 0xffffffff;
+        lowestByte = dword & 0b11111111;
+        lowerByte = (dword >>> 8) & 0b11111111;
+        higherByte = (dword >>> 16) & 0b11111111;
+        highestByte = (dword >>> 24) & 0b11111111;
+        barr.push(lowestByte, lowerByte, higherByte, highestByte);
+    }
+    return barr;
 }
 
 function dwarr2barrB(dwarr) {
+    if (!dwarr) throw new Error(ERR020);
+    if (!Array.isArray(dwarr)) throw new Error(ERR040);
+    let barr = [];
+    for (let i = 0; i < dwarr.length; i++) {
+        let dword = dwarr[i] & 0xffffffff;
+        lowestByte = dword & 0b11111111;
+        lowerByte = (dword >>> 8) & 0b11111111;
+        higherByte = (dword >>> 16) & 0b11111111;
+        highestByte = (dword >>> 24) & 0b11111111;
+        barr.push(highestByte, higherByte, lowerByte, lowestByte);
+    }
+    return barr;
 }
 
 // Carr construction.
@@ -76,20 +173,20 @@ function carr2barrB(carr) {
 }
 
 function isConsistentCarr(carr) {
-	if(carr === null) return false;
-	if(!Array.isArray(carr)) return false;
-	let byteLen = 0;
-	if(carr.length > 0) {
-		let firstDwarr = carr[0];
-		if(!Array.isArray(firstDwarr)) return false;
-		byteLen = firstDwarr.length;
-	}
-	for(let i = 1; i < carr.length; i++) {
-		let dwarr = carr[i];
-		if(!Array.isArray(dwarr)) return false;
-		if (dwarr.length !== byteLen) return false;
-	}
-	return true;
+    if (carr === null) return false;
+    if (!Array.isArray(carr)) return false;
+    let byteLen = 0;
+    if (carr.length > 0) {
+        let firstDwarr = carr[0];
+        if (!Array.isArray(firstDwarr)) return false;
+        byteLen = firstDwarr.length;
+    }
+    for (let i = 1; i < carr.length; i++) {
+        let dwarr = carr[i];
+        if (!Array.isArray(dwarr)) return false;
+        if (dwarr.length !== byteLen) return false;
+    }
+    return true;
 }
 
 // Bit operations on dwarrs.
@@ -97,7 +194,7 @@ function isConsistentCarr(carr) {
 // * dwarr is is considered to be big endian here.
 
 function isCompatibleDwarrs(op1, op2) {
-	return op1 !== null && op2 !== null && Array.isArray(op1) && Array.isArray(op2) && op1.length === op2.length;
+    return op1 !== null && op2 !== null && Array.isArray(op1) && Array.isArray(op2) && op1.length === op2.length;
 }
 
 function dwarrAnd(op1, op2) {
@@ -154,4 +251,6 @@ function blocks2barr(blocks) {
 export {
     barr2rstr as barr2rstr,
     rstr2barr as rstr2barr,
+    barr2warrL as barr2warrL,
+    barr2warrB as barr2warrB,
 }
--- test/bytes.test.js
+++ test/bytes.test.js
@@ -1,16 +1,67 @@
-import {rstr2barr as rstr2barr} from 'bytes';
+import {
+    rstr2barr as rstr2barr,
+    barr2rstr as barr2rstr,
+    barr2warrL as barr2warrL,
+    barr2warrB as barr2warrB,
+} from 'bytes';
 
 test('rstr2barr', () => {
-   let rstr = "abcdef";
-   let barr = rstr2barr(rstr);
-   expect(barr.length).toBe(6);
-   expect(barr).toEqual([97, 98, 99, 100, 101, 102]);
+    let rstr = "abcdef";
+    let barr = rstr2barr(rstr);
+    expect(barr.length).toBe(6);
+    expect(barr).toEqual([97, 98, 99, 100, 101, 102]);
 
-   rstr = '\u0000\u0001\u0002\u00ff\u0100\uff00'
-   barr = rstr2barr(rstr);
-   expect(barr.length).toBe(6);
-   expect(barr).toEqual([0, 1, 2, 255, 0, 0]);
+    rstr = '\u0000\u0001\u0002\u00ff\u0100\uff00';
+    barr = rstr2barr(rstr);
+    expect(barr.length).toBe(6);
+    expect(barr).toEqual([0, 1, 2, 255, 0, 0]);
 
+    expect(() => rstr2barr(null)).toThrow(/Bytes\/010/);
+    expect(() => rstr2barr({})).toThrow(/Bytes\/030/);
+
+    let rstr2 = "";
+    for(let i = 0; i < 256; i++) rstr2 += (i %10);
+    let barr2 = rstr2barr(rstr2);
+    expect(barr2.length).toBe(256);
 });
 
+test('barr2rstr', () => {
+    let barr = [0, 1, 2, 3, 13, 256];
+    let rstr = barr2rstr(barr);
+
+    expect(rstr.length).toBe(6);
+    expect(rstr).toBe("\u0000\u0001\u0002\u0003\u000d\u0000");
+
+    expect(() => barr2rstr(null)).toThrow(/Bytes\/020/);
+    expect(() => barr2rstr({})).toThrow(/Bytes\/040/);
+
+    let barr2 = [];
+    for(let i = 0; i < 256; i++) barr2.push(i);
+    let rstr2 = barr2rstr(barr2);
+    expect(rstr2.length).toBe(256);
+});
+
+test('barr2warrL', () => {
+    let barr = [0x05, 0x07, 0x0b, 0x0d, 0x010a, 0x010b];
+    let warr = barr2warrL(barr);
+
+    expect(warr.length).toBe(3);
+    expect(warr).toEqual([0x0705, 0x0d0b, 0x0b0a]);
+
+    expect(() => barr2warrL(null)).toThrow(/Bytes\/020/);
+    expect(() => barr2warrL({})).toThrow(/Bytes\/040/);
+    expect(() => barr2warrL([1, 2, 3])).toThrow(/Bytes\/050/);
+});
+
+test('barr2warrB', () => {
+    let barr = [0x05, 0x07, 0x0b, 0x0d, 0x010a, 0x010b];
+    let warr = barr2warrB(barr);
+
+    expect(warr.length).toBe(3);
+    expect(warr).toEqual([0x0507, 0x0b0d, 0x0a0b]);
+
+    expect(() => barr2warrB(null)).toThrow(/Bytes\/020/);
+    expect(() => barr2warrB({})).toThrow(/Bytes\/040/);
+    expect(() => barr2warrB([1, 2, 3])).toThrow(/Bytes\/050/);
+});
 
